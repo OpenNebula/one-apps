b40dadab0adf5b3304d9e3cab8f402e26f50c4ec
Michal Opala <mopala@opennebula.io>
2024-01-29 17:02:06 +0100

    M #-: OPENSUSE image cleanup + context lookup fix

--- packer/opensuse/11-update-grub.sh
+++ packer/opensuse/11-update-grub.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
-# Sets kernel command line (net.ifnames=0 is particularily important),
-# then updates grub2.
+# Set kernel command line (net.ifnames=0 is particularily important),
+# then update initramfs/initrd and grub2.
 
 exec 1>&2
-set -o errexit -o nounset -o pipefail
-set -x
+set -eux -o pipefail
 
 rm -rf /etc/default/grub.d/
 
 # Drop unwanted.
 
 gawk -i inplace -f- /etc/default/grub <<'EOF'
-/^GRUB_CMDLINE_LINUX[^=]*=/ { gsub(/\<quiet\>/, "") }
-/^GRUB_CMDLINE_LINUX[^=]*=/ { gsub(/\<splash\>/, "") }
-/^GRUB_CMDLINE_LINUX[^=]*=/ { gsub(/\<console=ttyS[^ ]*\>/, "") }
-/^GRUB_CMDLINE_LINUX[^=]*=/ { gsub(/\<earlyprintk=ttyS[^ ]*\>/, "") }
-/^GRUB_CMDLINE_LINUX[^=]*=/ { gsub(/\<crashkernel=[^ ]*\>/, "crashkernel=no") }
+/^GRUB_CMDLINE_LINUX=/ { gsub(/\<quiet\>/, "") }
+/^GRUB_CMDLINE_LINUX=/ { gsub(/\<splash\>/, "") }
+/^GRUB_CMDLINE_LINUX=/ { gsub(/\<console=ttyS[^ ]*\>/, "") }
+/^GRUB_CMDLINE_LINUX=/ { gsub(/\<earlyprintk=ttyS[^ ]*\>/, "") }
+/^GRUB_CMDLINE_LINUX=/ { gsub(/\<crashkernel=[^ ]*\>/, "crashkernel=no") }
 { print }
 EOF
 
@@ -27,14 +26,14 @@ gawk -i inplace -f- /etc/default/grub <<'EOF'
 /^GRUB_CMDLINE_LINUX=/ && !/net.ifnames=0/ { gsub(/"$/, " net.ifnames=0\"") }
 /^GRUB_CMDLINE_LINUX=/ && !/biosdevname=0/ { gsub(/"$/, " biosdevname=0\"") }
 { print }
-END { if (!found) print "GRUB_CMDLINE_LINUX=\" net.ifnames=0 biosdevname=0\"" >> FILENAME }
+ENDFILE { if (!found) print "GRUB_CMDLINE_LINUX=\" net.ifnames=0 biosdevname=0\"" }
 EOF
 
 gawk -i inplace -f- /etc/default/grub <<'EOF'
 BEGIN { update = "GRUB_TIMEOUT=0" }
 /^GRUB_TIMEOUT=/ { $0 = update; found = 1 }
 { print }
-END { if (!found) print update >> FILENAME }
+ENDFILE { if (!found) print update }
 EOF
 
 # Cleanup.
