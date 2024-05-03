ab43b14df2b0316af44fce579c2618da37f3abeb
Lukáš Fázik <lukas.faziklf@gmail.com>
2024-05-03 12:37:44 +0200

    Fix invalid destination path when copying Run-Scripts.ps1 in Copy-Scritps.ps1 and remove Unnecessary return value
    Signed-off-by: Lukáš Fázik <lukas.faziklf@gmail.com>

--- packer/windows/scripts/audit/98-Copy-Scripts.ps1
+++ packer/windows/scripts/audit/98-Copy-Scripts.ps1
@@ -1,3 +1,2 @@
 Copy-Item -Recurse -Force A:\scripts\oobe $env:systemdrive\scripts
-Copy-Item -Force A:\Run-Scripts.ps1 $env:systemdrive
-exit 0
+Copy-Item -Force A:\Run-Scripts.ps1 $env:systemdrive\Run-Scripts.ps1
