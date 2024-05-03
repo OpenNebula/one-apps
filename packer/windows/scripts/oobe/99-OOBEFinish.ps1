be81c4662205cb4b029282cd3e17cc6c2981e9ee
Lukáš Fázik <lukas.faziklf@gmail.com>
2024-05-03 15:52:14 +0200

    Fix Remove-Item paths in OOBEFinish.ps1
    Signed-off-by: Lukáš Fázik <lukas.faziklf@gmail.com>

--- packer/windows/scripts/oobe/99-OOBEFinish.ps1
+++ packer/windows/scripts/oobe/99-OOBEFinish.ps1
@@ -1,4 +1,3 @@
-Remove-Item -Recurse -Force $env:systemdrive\scripts %$env:systemdrive\Unattend.xml $env:windir\Panther\Unattend.xml
-Remove-Item -Force $env:systemdrive\Run-Scripts.ps1
+Remove-Item -Recurse -Force $env:systemdrive\scripts, `%$env:systemdrive\Unattend.xml, $env:windir\Panther\Unattend.xml, $env:systemdrive\Run-Scripts.ps1
 logoff.exe
 exit 0
