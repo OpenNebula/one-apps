a407257b7bc6afa8a4bdfe30b7588154504d625c
Lukáš Fázik <lukas.faziklf@gmail.com>
2024-05-03 11:05:27 +0200

    Change Start-Process to call operator in Install-Drivers.ps1
    According to best practices
    
    Signed-off-by: Lukáš Fázik <lukas.faziklf@gmail.com>

--- packer/windows/scripts/specialize/00-Install-Drivers.ps1
+++ packer/windows/scripts/specialize/00-Install-Drivers.ps1
@@ -1,11 +1,7 @@
 # Get the drive letter for the CD-ROM drive with VirtIO drivers
 $DriveLetter = Get-WmiObject Win32_CDRomDrive | Where-Object VolumeName -like virtio-win-* | Select-Object -ExpandProperty Drive
-# Prepare arguments for Start-Process
-$ProcessOptions = @{
-    "FilePath"      = "$DriveLetter\virtio-win-guest-tools.exe";
-    "ArgumentList" = @("/install", "/quiet");
-    "Wait"          = $true
-}
-# Run te installer
-Start-Process @ProcessOptions
+# Get the installer executable path
+$InstallerExecutablePath = "$DriveLetter\virtio-win-guest-tools.exe"
+# Run the installer
+& $InstallerExecutablePath /install /quiet
 exit 0
