cf9d659b6c5aaa610651f3458f81da85bb492748
Daniel <dgarcia@opennebula.io>
2024-05-06 15:52:48 +0200

    SSL certs fix
    Signed-off-by: Daniel <dgarcia@opennebula.io>

--- appliances/Harbor/appliance.sh
+++ appliances/Harbor/appliance.sh
@@ -70,8 +70,8 @@ service_configure() {
             generate_ssl_certs
         else
             msg info "Configuring provided SSL certificates..."
-            echo $HARBOR_SSL_CERT >> mkdir /root/certs/server.crt
-            echo $HARBOR_SSL_KEY >> mkdir /root/certs/server.key
+            echo $HARBOR_SSL_CERT >> /root/certs/server.crt
+            echo $HARBOR_SSL_KEY >> /root/certs/server.key
         fi
     else
         msg info "The certificates already exist"
