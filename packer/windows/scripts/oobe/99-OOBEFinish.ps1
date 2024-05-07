4ed484aa023a6861bb6533aec262290c3711e8df
Lukáš Fázik <lukas.faziklf@gmail.com>
2024-05-07 14:10:49 +0200

    Fix Unattend path in OOBEFinish.ps1
    Signed-off-by: Lukáš Fázik <lukas.faziklf@gmail.com>

--- packer/windows/scripts/oobe/99-OOBEFinish.ps1
+++ packer/windows/scripts/oobe/99-OOBEFinish.ps1
@@ -1,3 +1,3 @@
-Remove-Item -Recurse -Force $env:systemdrive\scripts, %$env:systemdrive\Unattend.xml, $env:windir\Panther\Unattend.xml, $env:systemdrive\Run-Scripts.ps1
+Remove-Item -Recurse -Force $env:systemdrive\scripts, $env:systemdrive\Unattend.xml, $env:windir\Panther\Unattend.xml, $env:systemdrive\Run-Scripts.ps1
 logoff.exe
 exit 0
