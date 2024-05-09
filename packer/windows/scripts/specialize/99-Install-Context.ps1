8092f4d6056f10cba98c723a356857d88f27d557
Lukáš Fázik <lukas.faziklf@gmail.com>
2024-05-09 15:10:47 +0200

    Refactor Install-Context.ps1
    Signed-off-by: Lukáš Fázik <lukas.faziklf@gmail.com>

--- packer/windows/scripts/specialize/99-Install-Context.ps1
+++ packer/windows/scripts/specialize/99-Install-Context.ps1
@@ -1,11 +1,5 @@
 # Get the path to the MSI installer
 $InstallerPath = Get-ChildItem -Path "A:\one-context-*.msi" | Select-Object -ExpandProperty FullName
-# Prepare arguments for Start-Process
-$ProcessOptions = @{
-    "FilePath"      = "msiexec.exe";
-    "ArgumentList" = @("/i",$InstallerPath, "/qn");
-    "Wait"          = $true
-}
 # Run msiexec
-Start-Process @ProcessOptions
+& msiexec.exe /i $InstallerPath /qn
 exit 0
