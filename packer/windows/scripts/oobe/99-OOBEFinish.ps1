6a321876b6ae8d14eeadbcaec0d3a696b13f6128
Lukáš Fázik <lukas.faziklf@gmail.com>
2024-05-12 18:13:47 +0200

    Add check for OOBE  finish status in OOBEFinish.ps1
    Now the user is logged out only after OOBE finishes
    
    Signed-off-by: Lukáš Fázik <lukas.faziklf@gmail.com>

--- packer/windows/scripts/oobe/99-OOBEFinish.ps1
+++ packer/windows/scripts/oobe/99-OOBEFinish.ps1
@@ -1,3 +1,20 @@
+# Delete unattend files and scripts
 Remove-Item -Recurse -Force $env:systemdrive\scripts, $env:systemdrive\Unattend.xml, $env:windir\Panther\Unattend.xml, $env:systemdrive\Run-Scripts.ps1
+# Wait for OOBE to finish provisioning apps (finished when user desktop shows up)
+$RegistryKeyPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE\Stats"
+$RegistryKeyName = "OOBEUserSignedIn2"
+$OOBEFinished = $false
+while ($OOBEFinished -eq $false) {
+    try {
+        Get-ItemProperty -Path $RegistryKeyPath -Name $RegistryKeyName -ErrorAction Stop | Out-Null
+        $OOBEFinished = $true
+    }
+    catch {
+        Start-Sleep -Seconds 1
+    }
+}
+# logoff user 5 seconds after OOBE finishes
+Start-Sleep 5
 logoff.exe
+# exit script withour restart
 exit 0
