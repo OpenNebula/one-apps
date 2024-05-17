d2d5d060efc114737087f29f1410fdd1f99687ec
Lukáš Fázik <lukas.faziklf@gmail.com>
2024-05-17 11:05:11 +0200

    Fix asynchronous msiexec invocation in Install-Context.ps1
    Replace call operator with Start-Process and -Wait parameter
    
    Signed-off-by: Lukáš Fázik <lukas.faziklf@gmail.com>

--- packer/windows/scripts/specialize/99-Install-Context.ps1
+++ packer/windows/scripts/specialize/99-Install-Context.ps1
@@ -1,5 +1,5 @@
 # Get the path to the MSI installer
 $InstallerPath = Get-ChildItem -Path "A:\one-context-*.msi" | Select-Object -ExpandProperty FullName
 # Run msiexec
-& msiexec.exe /i $InstallerPath /qn
+Start-Process -FilePath msiexec.exe -ArgumentList "/i", $InstallerPath, "/qn" -Wait
 exit 0
