ad03dd344a81c2de1759c10e5fad5f4118724d2a
Aleix Ramírez <aramirez@opennebula.io>
2024-10-23 15:58:59 +0200

    F#129: code linting
    Signed-off-by: Aleix Ramírez <aramirez@opennebula.io>

--- context-linux/src/usr/bin/one_cloudinit.rb
+++ context-linux/src/usr/bin/one_cloudinit.rb
@@ -20,166 +20,177 @@ require 'psych'
 require 'logger'
 
 ##
-# The CloudInit module implements cloud-init features for OpenNebula 
+# The CloudInit module implements cloud-init features for OpenNebula
 # contextualization.
 ##
 module CloudInit
 
-  Logger = Logger.new(
-    STDOUT, 
-    level: Logger::INFO,
-    formatter: proc {|severity, datetime, progname, msg|
-      date_format = datetime.strftime("%Y-%m-%d %H:%M:%S")
-      "[#{date_format} - #{severity}]: #{msg}\n"
-  })
+    Logger = Logger.new(
+        STDOUT,
+        :level => Logger::INFO,
+        :formatter => proc {|severity, datetime, _progname, msg|
+            date_format = datetime.strftime('%Y-%m-%d %H:%M:%S')
+            "[#{date_format} - #{severity}]: #{msg}\n"
+        }
+    )
 
-  def self.printException(exception, prefix="Exception")
-    CloudInit::Logger.error("#{prefix}: #{exception.message}\n#{exception.backtrace.join("\n")}")
-  end
-
-  def self.load_cloud_config_from_user_data
-    user_data = ENV['USER_DATA']
-
-    if user_data.nil?
-      Logger.info("No USER_DATA found.")
-      return nil
-    end
-    CloudConfig.from_yaml(user_data)
-  end
-
-  ##
-  # The CloudConfig module contains the functionality for parsing and executing
-  # cloud-config YAML files.
-  ##
-  class CloudConfig
-    attr_accessor :write_files, :runcmd
-
-    def initialize(write_files: [], runcmd: [])
-      @write_files = write_files
-      @runcmd = runcmd
+    def self.printException(exception, prefix = 'Exception')
+        CloudInit::Logger.error("#{prefix}: #{exception.message}\n#{exception.backtrace.join("\n")}")
     end
 
-    def self.from_yaml(yaml_string)
-      begin
-        parsed_cloud_config = Psych.safe_load(yaml_string, symbolize_names: true)
-      rescue Psych::SyntaxError => e
-        raise "YAML parsing failed: #{e.backtrace}"
-      end
+    def self.load_cloud_config_from_user_data
+        user_data = ENV['USER_DATA']
 
-      write_files = CloudConfigList.new(parsed_cloud_config[:write_files], WriteFile.method(:from_map))
-      runcmd = CloudConfigList.new(parsed_cloud_config[:runcmd], RunCmd.method(:from_map))
-
-      return new(write_files: write_files, runcmd: runcmd)
-    end
-
-    def exec
-      instance_variables.each do |var|
-        cloudconfig_directive = instance_variable_get(var)
-        if !cloudconfig_directive.respond_to?(:exec)
-          #TODO: Raise a not implemented exception or just ignore?
-          return
+        if user_data.nil?
+            Logger.info('No USER_DATA found.')
+            return
         end
-        cloudconfig_directive.exec
-      end
+        CloudConfig.from_yaml(user_data)
     end
 
-    class CloudConfig::CloudConfigList
-        attr_accessor :cloud_config_list
+    ##
+    # The CloudConfig module contains the functionality for parsing and executing
+    # cloud-config YAML files.
+    ##
+    class CloudConfig
 
-        def initialize(data_map, mapping_method)
-          @cloud_config_list = data_map.map do |element|
-            #TODO: validate
-            mapping_method.call(element)
-          end
+        attr_accessor :write_files, :runcmd
+
+        def initialize(write_files: [], runcmd: [])
+            @write_files = write_files
+            @runcmd = runcmd
         end
 
-        def validate
-          # TODO
+        def self.from_yaml(yaml_string)
+            begin
+                parsed_cloud_config = Psych.safe_load(yaml_string, :symbolize_names => true)
+            rescue Psych::SyntaxError => e
+                raise "YAML parsing failed: #{e.backtrace}"
+            end
+
+            write_files = CloudConfigList.new(parsed_cloud_config[:write_files],
+                                              WriteFile.method(:from_map))
+            runcmd = CloudConfigList.new(parsed_cloud_config[:runcmd], RunCmd.method(:from_map))
+
+            return new(:write_files => write_files, :runcmd => runcmd)
         end
 
         def exec
-            @cloud_config_list.each do |element|
-              if !element.respond_to?(:exec)
-                #TODO: Not implemented exception?
-                return
-              end
-              element.exec
+            instance_variables.each do |var|
+                cloudconfig_directive = instance_variable_get(var)
+                if !cloudconfig_directive.respond_to?(:exec)
+                    # TODO: Raise a not implemented exception or just ignore?
+                    return
+                end
+
+                cloudconfig_directive.exec
             end
         end
+
+        class CloudConfig::CloudConfigList
+
+            attr_accessor :cloud_config_list
+
+            def initialize(data_map, mapping_method)
+                @cloud_config_list = data_map.map do |element|
+                    # TODO: validate
+                    mapping_method.call(element)
+                end
+            end
+
+            def validate
+                # TODO
+            end
+
+            def exec
+                @cloud_config_list.each do |element|
+                    if !element.respond_to?(:exec)
+                        # TODO: Not implemented exception?
+                        return
+                    end
+
+                    element.exec
+                end
+            end
+
+        end
+
+        class CloudConfig::WriteFile
+
+            attr_accessor :path, :content, :source, :owner, :permissions, :encoding, :append, :defer
+
+            def initialize(path:, content: '', source: [], owner: 'root:root',
+                           permissions: '0o644', encoding: 'text/plain', append: false,
+                           defer: false)
+                @path = path
+                @content = content
+                @source = source
+                @owner = owner
+                @permissions = permissions
+                @encoding = encoding
+                @append = append
+                @defer = defer
+            end
+
+            def validate
+                # TODO
+            end
+
+            def exec
+                # TODO
+                Logger.info("[writeFile] writing file #{@owner}-#{@permissions}@#{@path}:")
+            end
+
+            def self.from_map(data_map)
+                # TODO: Validate
+                WriteFile.new(**data_map)
+            end
+
+        end
+
+        class CloudConfig::RunCmd
+
+            attr_accessor :cmd
+
+            def initialize(cmd:)
+                @cmd = cmd
+            end
+
+            def validate
+                # TODO
+            end
+
+            def exec
+                # TODO
+                Logger.info("[runCmd] executing #{@cmd}")
+            end
+
+            def self.from_map(data_map)
+                # TODO: Validate
+                RunCmd.new(:cmd => data_map)
+            end
+
+        end
+
     end
 
-    class CloudConfig::WriteFile
-      attr_accessor :path, :content, :source, :owner, :permissions, :encoding, :append, :defer 
-  
-      def initialize(path:, content:"", source:[], owner:'root:root', 
-          permissions:'0o644', encoding:'text/plain', append:false, 
-          defer:false)
-        @path = path
-        @content = content
-        @source = source
-        @owner = owner
-        @permissions = permissions
-        @encoding = encoding
-        @append = append
-        @defer = defer
-      end
-  
-      def validate
-        #TODO
-      end
-
-      def exec
-        #TODO
-        Logger.info("[writeFile] writing file #{@owner}-#{@permissions}@#{@path}:")
-      end
-
-      def self.from_map(data_map)
-        #TODO: Validate
-        WriteFile.new(**data_map)
-      end
-    end
-
-    class CloudConfig::RunCmd
-      attr_accessor :cmd
-  
-      def initialize(cmd:)
-        @cmd = cmd
-      end
-  
-      def validate
-        #TODO
-      end
-
-      def exec
-        #TODO
-        Logger.info("[runCmd] executing #{@cmd}")
-      end
-
-      def self.from_map(data_map)
-        #TODO: Validate
-        RunCmd.new(cmd: data_map)
-      end
-      
-    end
-
-  end
 end
 
 # script start
 begin
-cloud_config = CloudInit.load_cloud_config_from_user_data()
+    cloud_config = CloudInit.load_cloud_config_from_user_data
 rescue Exception => e
-  CloudInit::printException(e, "could not parse USER_DATA")
-  exit 1
+    CloudInit.printException(e, 'could not parse USER_DATA')
+    exit 1
 end
 
 if cloud_config.nil?
-  exit 0
+    exit 0
 end
 
 begin
-  cloud_config.exec()
+    cloud_config.exec
 rescue Exception => e
-  CloudInit::printException(e, "error executing cloud-config")
-  exit 1
+    CloudInit.printException(e, 'error executing cloud-config')
+    exit 1
 end
