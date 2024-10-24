013b1689ed236c6ce1a5862240224e1dc51d495a
Jaime <jconchello@opennebula.io>
2024-10-24 17:01:53 +0200

    F#129 WriteFile first version
    Signed-off-by: Jaime <jconchello@opennebula.io>

--- context-linux/src/usr/bin/cloudinit_cc_write_files.rb
+++ context-linux/src/usr/bin/cloudinit_cc_write_files.rb
@@ -16,6 +16,8 @@
 # limitations under the License.                                             #
 #--------------------------------------------------------------------------- #
 
+require 'etc'
+
 module CloudInit
 
     ##
@@ -26,7 +28,7 @@ module CloudInit
         attr_accessor :path, :content, :source, :owner, :permissions, :encoding, :append, :defer
 
         def initialize(path:, content: '', source: [], owner: 'root:root',
-                       permissions: '0o644', encoding: 'text/plain', append: false,
+                       permissions: '0644', encoding: 'text/plain', append: false,
                        defer: false)
             @path = path
             @content = content
@@ -39,8 +41,37 @@ module CloudInit
         end
 
         def exec
-            # TODO: implement logic
-            puts '[writeFile] writing file'
+            begin
+                CloudInit::Logger.info(
+                    "[writeFile] writing file [#{@permissions} #{@owner} #{@path}]"
+                )
+
+                unless valid_octal?
+                    CloudInit::Logger.error(
+                        "[writeFile] Invalid permission [#{@permissions} #{@owner} #{@path}]"
+                    )
+                    raise ArgumentError
+                end
+
+                uid, gid = uid_and_guid_by_owner
+                File.open(@path, 'w') do |file|
+                    file.write(@content)
+                    file.chmod(@permissions.to_i(8))
+                    file.chown(uid, gid)
+                end
+            rescue Errno::EACCES
+                CloudInit::Logger.error(
+                    "[writeFile] Insufficient permissions [#{@permissions} #{@owner} #{@path}]"
+                )
+            rescue Errno::ENOENT
+                CloudInit::Logger.error(
+                    "[writeFile] Parent directory missing [#{@permissions} #{@owner} #{@path}]"
+                )
+            rescue StandardError => e
+                CloudInit::Logger.error(
+                    "[writeFile] Unexpected error: #{e.message}\n#{e.backtrace.join("\n")}"
+                )
+            end
         end
 
         def self.from_map(data_map)
@@ -49,6 +80,22 @@ module CloudInit
             end
 
             WriteFile.new(**data_map)
+        end
+
+        def valid_octal?
+            @permissions.match?(/\A0[0-7]{1,3}\z/)
+        end
+
+        def uid_and_guid_by_owner
+            user, group = @owner.split(':')
+
+            begin
+                return Etc.getpwnam(user).uid, Etc.getgrnam(group).gid
+            rescue ArgumentError
+                CloudInit::Logger.error(
+                    "[writeFile] Owner does not exist [#{@permissions} #{@owner} #{@path}]"
+                )
+            end
         end
 
     end
