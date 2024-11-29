d1fd6f4855b30b415dec0e45e51771b2d8f26336
Lukáš Fázik <lukas.faziklf@gmail.com>
2024-11-29 14:45:09 +0100

    Increase priority in Optimize-Volume cmdlet call
    Signed-off-by: Lukáš Fázik <lukas.faziklf@gmail.com>

--- packer/windows/scripts/audit/99-Optimize-Image.ps1
+++ packer/windows/scripts/audit/99-Optimize-Image.ps1
@@ -3,5 +3,5 @@ Write-Host "Optimizing the image"
 & dism.exe /online /Quiet /NoRestart /Cleanup-Image /StartComponentCleanup /ResetBase
 # Trim the disk
 Write-Host "Trimming the disk"
-Get-Volume | Optimize-Volume -ReTrim -SlabConsolidate -ErrorAction SilentlyContinue
+Get-Volume | Optimize-Volume -ReTrim -SlabConsolidate -NormalPriority -ErrorAction SilentlyContinue
 exit 0
