82d61336c406801988989f434cb0759eb572e83e
Lukáš Fázik <lukas.faziklf@gmail.com>
2024-12-05 17:51:55 +0100

    Use net user command to disable Administrator account in OOBEFinish script
    Ensures backward compatibility with PowerShell 5.0 in Windows 10 Enterprise LTSB 2015 (missing Disable-LocalUser cmdlet)
    
    Signed-off-by: Lukáš Fázik <lukas.faziklf@gmail.com>

--- packer/windows/scripts/oobe/99-OOBEFinish.ps1
+++ packer/windows/scripts/oobe/99-OOBEFinish.ps1
@@ -44,7 +44,7 @@ Set-Service -Name "onecontext" -StartupType Automatic
 Start-Service -Name "onecontext"
 # logoff user 5 seconds after OOBE finishes
 Start-Sleep 5
-Disable-LocalUser -Name Administrator
+net user Administrator /active:no
 shutdown.exe /l
 # exit script withour restart
 exit 0
