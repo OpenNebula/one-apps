a02cea6dc8ea056d4375914a5e4e30665118c29c
Michal Opala <mopala@opennebula.io>
2025-01-09 16:26:44 +0100

    Trigger reboot after updates if required

--- packer/windows/scripts/audit/97-Install-Updates.ps1
+++ packer/windows/scripts/audit/97-Install-Updates.ps1
@@ -1,1 +1,2 @@
 powershell.exe -ExecutionPolicy Bypass -NonInteractive -NoProfile -WindowStyle Maximized -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/rgl/packer-plugin-windows-update/c3721215ced494a4e18c3145110513de06c5e24e/update/windows-update.ps1'))"
+if ((New-Object -ComObject 'Microsoft.Update.SystemInfo').RebootRequired) {exit 2}
