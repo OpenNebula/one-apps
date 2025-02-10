3af5332f6ed9395e9b45213913d87b8c65335b23
Carlos Moral Rubio <carlos@MacBook-Pro-de-Carlos.local>
2025-02-10 21:20:33 +0100

    Added conversation skill to the default model.py and model_vllm.py

--- appliances/Ray/models/model.py
+++ appliances/Ray/models/model.py
@@ -14,13 +14,27 @@ app = FastAPI()
 @serve.ingress(app)
 class ChatBot:
     def __init__(self, model_id: str, token:str, temperature: float, system_prompt: str):
-        # Load model
-        self.device    = "cuda:0" if torch.cuda.is_available() else "cpu"
-        self.tokenizer = AutoTokenizer.from_pretrained(model_id, token=token)
-        self.model     = AutoModelForCausalLM.from_pretrained(model_id, token=token).to(self.device)
-        self.temperature   = temperature
-        self.system_prompt = system_prompt
+        """Default class for conversational chatbot using vLLM and Ray appliance.
+        Args:
+            model_id (str): HuggingFace model ID from the model that we want to deploy. 
+            token (str): HuggingFace token to be used for authentication.
+            temperature (str): temperature of the model, randomness.
+            system_prompt (str): system prompt to define the behaviour of the LLM.
 
+        Returns:
+            None.
+        """
+        # Load model
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_id, token=token)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_id, token=token).to(self.device)
+        self.temperature = temperature
+        self.system_prompt = system_prompt
+        self.messages = [
+                    {"role": "system", "content": self.system_prompt}]
+        self.nmessages_hist = 4
         # Check if there is chat_template
         if self.tokenizer.chat_template is None:
             self.tokenizer.chat_template = """
@@ -37,26 +51,37 @@ class ChatBot:
 
     @app.post("/chat")
     def chat(self, text: str) -> str:
-        messages = [
-            {"role": "system", "content": self.system_prompt},
-            {"role": "user", "content": text}]
+        """Endpoint to communicate with deployed LLM.
+        Args:
+            text (str): given user input.
+
+        Returns:
+            answer (str): response from the LLM.
+        """
+        self.messages.append(
+            {"role": "user", "content": text})
 
         # Tokenize the text
         chat = self.tokenizer.apply_chat_template(
-            messages, tokenize=False, add_generation_prompt=True, return_tensors="pt")
-
+            self.messages, tokenize=False, add_generation_prompt=True, return_tensors="pt")
         input_tokens = self.tokenizer(chat, return_tensors="pt").to(self.device)
 
         # Run inference
-        output = self.model.generate(**input_tokens, max_new_tokens=1024)
-
+        output = self.model.generate(
+            **input_tokens, 
+            max_new_tokens=1024)
+        
         # Decode output tokens into text
         prompt_length = input_tokens['input_ids'].shape[1]
-
         output = self.tokenizer.decode(output[0][prompt_length:])
         answer = re.sub(r'<.*?>', '', output)
 
+        # Add it to conversation
+        self.messages.append(
+            {"role": "assistant", "content": answer})
+        self.messages = self.messages[-self.nmessages_hist*2:]
         return answer
+    
 
 def app_builder(args: Dict[str, str]) -> Application:
     return ChatBot.bind(args["model_id"], args['token'], args['temperature'], args['system_prompt'])
--- appliances/Ray/models/model_vllm.py
+++ appliances/Ray/models/model_vllm.py
@@ -5,7 +5,6 @@ from ray.serve import Application
 from vllm import LLM, SamplingParams
 import os
 
-# include vllm in config.yaml for this configuration
 
 app = FastAPI()
 
@@ -13,24 +12,43 @@ app = FastAPI()
 @serve.ingress(app)
 class ChatBot:
     def __init__(self, model_id: str, token:str, temperature: float, system_prompt: str):
+        """Default class for conversational chatbot using vLLM and Ray appliance.
+        Args:
+            model_id (str): HuggingFace model ID from the model that we want to deploy. 
+            token (str): HuggingFace token to be used for authentication.
+            temperature (str): temperature of the model, randomness.
+            system_prompt (str): system prompt to define the behaviour of the LLM.
+
+        Returns:
+            None.
+        """
         # Set-up token env. to download models
         os.environ["HF_TOKEN"] = token
 
         # Load model
-        self.model = LLM(model=model_id, max_model_len=5000)
-
-        self.temperature   = temperature
+        self.model = LLM(
+            model=model_id, max_model_len=2048)
+        self.temperature = temperature
         self.system_prompt = system_prompt
-
+        self.nmessages_hist = 4
+        self.messages = [
+            {"role": "system", "content": self.system_prompt}]
+        
     @app.post("/chat")
     def chat(self, text: str) -> str:
-        messages = [
-            {"role": "system", "content": self.system_prompt},
-            {"role": "user", "content": text}]
+        """Endpoint to communicate with deployed LLM.
+        Args:
+            text (str): given user input.
 
+        Returns:
+            answer (str): response from the LLM.
+        """
+        self.messages.append(
+            {"role": "user", "content": text})
+    
         # Tokenize the text
         outputs = self.model.chat(
-            messages,
+            self.messages,
             sampling_params=SamplingParams(
                 temperature=self.temperature,
                 max_tokens=1024),
@@ -38,7 +56,12 @@ class ChatBot:
 
         # Run inference
         answer = outputs[0].outputs[0].text
+        # Add to conversation and strip it to 4 messages
+        self.messages.append(
+            {"role": "assistant", "content": answer})
+        self.messages = self.messages[-self.nmessages_hist*2:]
         return answer
+    
 
 def app_builder(args: Dict[str, str]) -> Application:
     return ChatBot.bind(args["model_id"], args['token'], args['temperature'], args['system_prompt'])
