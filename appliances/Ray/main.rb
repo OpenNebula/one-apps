0051f679b5844068850f0f8f8fc7877a942972dd
Ruben S. Montero <rsmontero@opennebula.org>
2025-03-25 22:20:55 +0100

    Missing dependency

--- appliances/Ray/main.rb
+++ appliances/Ray/main.rb
@@ -97,7 +97,7 @@ module Service
         puts bash <<~SCRIPT
             export DEBIAN_FRONTEND=noninteractive
             apt-get update
-            apt-get install -y python3 python3-pip
+            apt-get install -y python3 python3-pip python3-venv
             cd /root
             python3 -m venv ray_env
             source ray_env/bin/activate
